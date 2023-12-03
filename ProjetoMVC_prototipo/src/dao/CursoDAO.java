ca3e1f1b6c3ecce9c2c80950d645fe497d7004a4
Gabriel <gabriel.offrancisco@gmail.com>
2023-12-03 03:06:24 -0300

    Mudanças no DAO de curso e disciplina

--- ProjetoMVC_prototipo/src/dao/CursoDAO.java
+++ ProjetoMVC_prototipo/src/dao/CursoDAO.java
@@ -9,21 +9,40 @@ import model.CursoModel;
 import utils.ConnectionFactory;
 
 public class CursoDAO {
-	private Connection conn;   
-	private PreparedStatement ps;  
-	private ResultSet rs;  
-	
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+
 	private CursoModel cursoModel;
 
 	public CursoDAO() throws Exception {
-		
+
 		try {
 			this.conn = ConnectionFactory.getConnection();
 		} catch (Exception e) {
 			throw new Exception("erro: \n" + e.getMessage());
 		}
 	}
-	
+
 	public void salvarCurso(CursoModel cursoModel) throws Exception {
+		if (cursoModel == null)
+			throw new Exception("Os valores não podem ser nulos");
+
+		try {
+			String sql = "INSERT INTO curso (rgm, nome_curso, campus, periodo) VALUES (?, ?, ?, ?)";
+
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cursoModel.getRgm());
+	        ps.setString(2, cursoModel.getNomeCurso());
+	        ps.setString(3, cursoModel.getCampus());
+	        ps.setString(4, cursoModel.getPeriodo());
+
+			ps.executeUpdate();
+
+		} catch (SQLException sqle) {
+			throw new Exception("Erro ao inserir dados " + sqle);
+		} finally {
+			ConnectionFactory.closeConnection(conn, ps);
+		}
 	}
 }
--- ProjetoMVC_prototipo/src/dao/DisciplinaDAO.java
+++ ProjetoMVC_prototipo/src/dao/DisciplinaDAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import model.DisciplinaModel;
 import utils.ConnectionFactory;
@@ -23,6 +24,27 @@ public class DisciplinaDAO {
 		}
 	}
 	
-	public void salvar(DisciplinaModel disciplinaModel) throws Exception {
+	public void salvarDisciplina(DisciplinaModel disciplinaModel) throws Exception {
+		if (disciplinaModel == null)
+			throw new Exception("Os valores não podem ser nulos");
+
+		try {
+			String sql = "INSERT INTO curso (id_curso, rgm, nome_disciplina, semestre, notas, faltas) VALUES (?, ?, ?, ?, ?, ?)";
+
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, disciplinaModel.getIdCurso());
+			ps.setString(2, disciplinaModel.getRgm());
+			ps.setString(3, disciplinaModel.getNomeDisciplina());
+			ps.setString(4, disciplinaModel.getSemestre());
+			ps.setDouble(5, disciplinaModel.getNotas());
+			ps.setInt(6, disciplinaModel.getFaltas());
+			
+			ps.executeUpdate();
+
+		} catch (SQLException sqle) {
+			throw new Exception("Erro ao inserir dados " + sqle);
+		} finally {
+			ConnectionFactory.closeConnection(conn, ps);
+		}
 	}
 }
